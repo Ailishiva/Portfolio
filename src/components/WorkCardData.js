d536926a30ce15cbd2216b0f1cae249b19ebd938
Aili ShivaPrasad <112164679+Ailishiva@users.noreply.github.com>
2023-07-15 17:44:34 +0530

    Add files via upload

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,67 @@
+
+import pro1 from "../assets/project1.jpg";
+import pro2 from "../assets/project2.jpg";
+
+import pro3 from "../assets/project3.jpg";
+import pro4 from "../assets/project4.jpg";
+import pro6 from "../assets/project6.jpg";
+
+const ProjectCardData = [
+    {
+        imgsrc : pro4,
+        title : "IMDB Clone",
+        text : "This is a web application which allow users to search for and browse latest and upcoming movies.",
+        view : "https://imdb-clone-22f717.netlify.app"
+    },
+
+    {
+        imgsrc : pro3,
+        title : "YouTube-Clone",
+        text : " I created a YouTube clone using React, Rapid-API, and Material-UI. It features a responsive layout and allows users to search and play videos and pause and resume playback. This project was an excellent opportunity to learn . ",
+        view : "https://reactcloneyoutube.netlify.app/"
+    },
+
+    {
+        imgsrc : pro2,
+        title : "Meal Planner",
+        text : "A web application which takes user data and generates meal plan for them along with their recipes.",
+        view : "https://dreamy-paletas-36d5f5.netlify.app/"
+    },
+
+    {
+        imgsrc : pro1,
+        title : "Coffee Shop Website",
+        text : "Features Responsive home section with toggle menu effect using vanilla javascript.Responsive about section,  facility section, gallery section and footer section.",
+        view : "https://papaya-sunshine-c6a161.netlify.app/"
+    },
+    {
+      imgsrc : 'https://user-images.githubusercontent.com/65420004/180659905-25df4a63-6f62-4a09-85a5-86803d7796b2.jpg',
+      title : "Food - App",
+      text : "It is looking UI. A web Application is the Responsive and You can Filter it by prices and menu Items by using the React and Tailwind CSS",
+      view : "https://velvety-cocada-0c7a30.netlify.app/"
+    },
+
+    // {
+    //     imgsrc : pro5,
+    //     title : "Flower-shop Website",
+    //     text : "Features Responsive home section with toggle menu effect using HTML and CSS.Responsive about section,  facility section, gallery section and footer section.",
+    //     view : "https://flowerweb-766e0a.netlify.app"
+    // },
+
+    {
+        imgsrc : pro6,
+        title : " React-Admin-Dashboard",
+        text : "  The project's purpose was to create a user-friendly interface so we can add new users and view the details and delete them. ",
+        view: "https://project-syncfusion-dashboard-main-delta.vercel.app/"
+        //view : "https://zesty-salmiakki-a1eed4.netlify.app/"
+    }
+
+
+    
+
+    
+
+    
+];
+
+export default ProjectCardData;
